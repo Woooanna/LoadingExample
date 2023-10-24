1faafa227be11bda763084dfd49456fb7e42ba60
Yoanna <y.mareva@tinqin.com>
2023-10-24 12:03:34 +0300

    LoadingExample App init

---
+++ LoadingExample/LoadingExample/LoadingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingExampleApp.swift
+//  LoadingExample
+//
+//  Created by Yoanna Mareva on 24.10.23.
+//
+
+import SwiftUI
+
+@main
+struct LoadingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
